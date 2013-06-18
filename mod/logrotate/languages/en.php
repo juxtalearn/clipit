b8885dda0e29b271469ddcb2655d671472d5b81a
Pablo Llinás Arnaiz <pebs74@gmail.com>
2013-06-18 17:54:07 +0200

    Initial Elgg 1.8.15 Upload

---
+++ mod/logrotate/languages/en.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Elgg log rotator language pack.
+ *
+ * @package ElggLogRotate
+ */
+
+$english = array(
+	'logrotate:period' => 'How often should the system log be archived?',
+
+	'logrotate:weekly' => 'Once a week',
+	'logrotate:monthly' => 'Once a month',
+	'logrotate:yearly' => 'Once a year',
+
+	'logrotate:logrotated' => "Log rotated\n",
+	'logrotate:lognotrotated' => "Error rotating log\n",
+	
+	'logrotate:delete' => 'Delete archived logs older than a',
+
+	'logrotate:week' => 'week',
+	'logrotate:month' => 'month',
+	'logrotate:year' => 'year',
+	'logrotate:never' => 'never',
+		
+	'logrotate:logdeleted' => "Log deleted\n",
+	'logrotate:lognotdeleted' => "Error deleting log\n",
+);
+
+add_translation("en", $english);
