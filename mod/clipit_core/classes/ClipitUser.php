cf199a221d612a5638392c05df68e462ebf91785
Miguel Ángel <magutierrezmoreno@gmail.com>
2013-10-21 12:30:36 +0200

    edit

--- mod/clipit_core/classes/ClipitUser.php
+++ mod/clipit_core/classes/ClipitUser.php
@@ -33,36 +33,43 @@
  */
 
 class ClipitUser{
-    
+
     // Class properties
-    public $avatar;
-    public $description;
-    public $email;
-    public $name;
-    public $id;
-    public $login;
-    public $password;
-    public $role;
-    public $time_created;
+    public $id = -1;
+    public $login = "";
+    public $password = "";
+    public $password_hash = "";
+    public $description = "";
+    public $email = "";
+    public $name = "";
+    public $role = "";
+    public $time_created = -1;
 
     function __construct($id = null){
-        $this->avatar = ""; //@todo insert ClipitFile instance
-        $this->description = "";
-        $this->email = "";
-        $this->name = "";
-        $this->id = -1;
-        $this->login = "";
-        $this->password = "";
-        $this->role = "basic";
-        $this->time_created = -1;
-        if(!$id){
+        $this->load($id);
+    }
+
+    function save(){
+        if($this->id == -1){
             $elgg_user = new ElggUser();
             $id = $elgg_user->save();
             $this->id = $id;
             $this->login = "user_".$id;
             $this->save();
+        } else{
+            $elgg_user = new ElggUser($this->id);
         }
-        $this->load($id);
+        if(!$elgg_user){
+            return false;
+        }
+        $elgg_user->set("description", $this->description);
+        $elgg_user->set("email", $this->email);
+        $elgg_user->set("name", $this->name);
+        $elgg_user->set("username", $this->login);
+        $elgg_user->set("password", $this->password);
+        $elgg_user->set("salt", $this->password_hash);
+        $elgg_user->set("role", $this->role);
+        return $elgg_user->save();
     }
 
     function load($id){
@@ -70,191 +77,19 @@ class ClipitUser{
         if(!$elgg_user || !is_a($elgg_user, "ElggUser")){
             return null;
         }
-        $this->avatar = $elgg_user->get("avatar");
         $this->description = $elgg_user->get("description");
         $this->email = $elgg_user->get("email");
         $this->name = $elgg_user->get("name");
         $this->id = $elgg_user->get('guid');
         $this->login = $elgg_user->get("username");
         $this->password = $elgg_user->get("password");
+        $this->password_hash = $elgg_user->salt;
         $this->role = $elgg_user->get("role");
         $this->time_created = $elgg_user->get("time_created");
         return $this;
     }
 
-    function save(){
-        $elgg_user = new ElggUser($this->id);
-        if(!$elgg_user){
-            return false;
-        }
-        $elgg_user->set("avatar", $this->avatar);
-        $elgg_user->set("description", $this->description);
-        $elgg_user->set("email", $this->email);
-        $elgg_user->set("name", $this->name);
-        $elgg_user->set("username",$this->login);
-        $elgg_user->set("password", $this->password);
-        $elgg_user->set("role", $this->role);
-        return $elgg_user->save();
-    }
+    function delete(){
 
-    static function exposeFunctions(){
-        expose_function("clipit.user.getProperties",
-            "ClipitUser::getProperties",
-            array(
-                "id" => array(
-                    "type" => "int",
-                    "required" => true),
-                "prop_array" => array(
-                    "type" => "array",
-                    "required" => true)),
-            "<description>",
-            'GET',
-            true,
-            false);
-        expose_function("clipit.user.setProperties",
-            "ClipitUser::setProperties",
-            array(
-                "id" => array(
-                    "type" => "int",
-                    "required" => true),
-                "prop_array" => array(
-                    "type" => "array",
-                    "required" => true),
-                "value_array" => array(
-                    "type" => "array",
-                    "required" => true)),
-            "<description>",
-            'GET',
-            true,
-            false);
-        expose_function("clipit.user.getAllUsers",
-            "ClipitUser::getAllUsers",
-            NULL,
-            "<description>",
-            'GET',
-            true,
-            false);
-        expose_function("clipit.user.getUsersById",
-            "ClipitUser::getUsersById",
-            array(
-                "id_array" => array(
-                    "type" => "array",
-                    "required" => true)),
-            "<description>",
-            'GET',
-            true,
-            false);
-        expose_function("clipit.user.getUsersByLogin",
-            "ClipitUser::getUsersByLogin",
-            array(
-                "login_array" => array(
-                    "type" => "array",
-                    "required" => true)),
-            "<description>",
-            'GET',
-            true,
-            false);
-        expose_function("clipit.user.getUsersByEmail",
-            "ClipitUser::getUsersByEmail",
-            array(
-                "email_array" => array(
-                    "type" => "array",
-                    "required" => true)),
-            "<description>",
-            'GET',
-            true,
-            false);
     }
-
-    static function getProperties($id, $prop_array){
-        $user = new ClipitUser($id);
-        if(!$user){
-            return null;
-        }
-        $value_array = array();
-        for($i=0; $i<count($prop_array); $i++){
-            $value_array[$i] = $user->$prop_array[$i];
-        }
-        return array_combine($prop_array, $value_array);
-    }
-
-    static function setProperties($id, $prop_array, $value_array){
-        if(count($prop_array)!=count($value_array)){
-            return null;
-        }
-        $user = new ClipitUser($id);
-        if(!$user){
-            return null;
-        }
-        for($i=0; $i<count($prop_array); $i++){
-            $user->$prop_array[$i] = $value_array[$i];
-        }
-        return $user->save();
-    }
-
-    static function getAllUsers(){
-        $elgg_user_array = elgg_get_entities(array('type'=>'user'));
-        $user_array = array();
-        for($i = 0; $i < count($elgg_user_array); $i++){
-            $user_array[$i] = new ClipitUser($elgg_user_array[$i]->get("guid"));
-        }
-        if(!$user_array){
-            return null;
-        }
-        return $user_array;
-    }
-
-    static function getUsersById($id_array){      
-        for($i = 0; $i < count($id_array); $i++){
-            $elgg_user = get_user($id_array[$i]);
-            if(!$elgg_user){
-                $user_array[$i] = null;
-                continue;
-            }
-            $user_array[$i] = new ClipitUser($elgg_user->get("guid"));
-        }
-        if(!$user_array){
-            return null;
-        }
-        return $user_array;
-    }
-
-    static function getUsersByLogin($login_array){
-        for($i = 0; $i < count($login_array); $i++){
-            $elgg_user = get_user_by_username($login_array[$i]);
-            if(!$elgg_user){
-                $user_array[$i] = null;
-                continue;
-            }
-            $user_array[$i] = new ClipitUser($elgg_user->get("guid"));
-        }
-        if(!$user_array){
-            return null;
-        }
-        return $user_array;
-    }
-
-    static function getUsersByEmail($email_array){
-        $user_array = array();
-        for($i = 0; $i < count($email_array); $i++){
-            $elgg_user_array = get_user_by_email($email_array[$i]);
-            if(!$elgg_user_array){
-                $user_array[$i] = null;
-                continue;
-            }
-            for($j = 0; $j < count($elgg_user_array); $j++){
-                $temp_array[$j] = new ClipitUser($elgg_user_array[$j]->get("guid"));
-            }
-            if(!$temp_array){
-                $user_array[$i] = null;
-                continue;
-            }
-            $user_array = array_merge($user_array, $temp_array);
-        }
-        if(!$user_array){
-            return null;
-        }
-        return $user_array;
-    }
-
 }
