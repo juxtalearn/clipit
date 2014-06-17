ff2e7b13ae247f1cccd1aca35c61b59cdcdc3f16
pebs74 <KrisYPebs2014>
2014-06-17 12:09:38 +0200

    load of performance palette in z02 start instead of activate (it didn't work)

--- mod/z02_clipit_api/start.php
+++ mod/z02_clipit_api/start.php
@@ -25,9 +25,9 @@ function clipit_api_init(){
     loadFiles(elgg_get_plugins_path() . "z02_clipit_api/libraries/juxtalearn-cookie-authentication/");
     loadFiles(elgg_get_plugins_path() . "z02_clipit_api/libraries/simple_xlsx/");
     expose_clipit_api();
-    rename_subtypes();
     elgg_register_page_handler('youtube_auth', 'youtube_auth_page_handler');
     elgg_register_page_handler("data_input", "data_input_page_handler");
+    load_performance_palette();
 }
 
 function youtube_auth_page_handler($page){
@@ -54,40 +54,40 @@ function data_input_page_handler($page){
     echo elgg_view_page($title, $body);
 }
 
-/**
- * Replace old subtypes with class names, for easier reference.
- */
-function rename_subtypes(){
-    global $CONFIG;
-    $replace["clipit_activity"] = ClipitActivity::SUBTYPE;
-    $replace["clipit_chat"] = ClipitChat::SUBTYPE;
-    $replace["clipit_comment"] = ClipitComment::SUBTYPE;
-    $replace["clipit_example"] = ClipitExample::SUBTYPE;
-    $replace["clipit_file"] = ClipitFile::SUBTYPE;
-    $replace["clipit_group"] = ClipitGroup::SUBTYPE;
-    $replace["clipit_la"] = ClipitLA::SUBTYPE;
-    $replace["clipit_performance_item"] = ClipitPerformanceItem::SUBTYPE;
-    $replace["clipit_performance_rating"] = ClipitPerformanceRating::SUBTYPE;
-    $replace["clipit_post"] = ClipitPost::SUBTYPE;
-    $replace["clipit_quiz"] = ClipitQuiz::SUBTYPE;
-    $replace["clipit_quiz_question"] = ClipitQuizQuestion::SUBTYPE;
-    $replace["clipit_quizquestion"] = ClipitQuizQuestion::SUBTYPE;
-    $replace["clipit_quiz_result"] = ClipitQuizResult::SUBTYPE;
-    $replace["clipit_quizresult"] = ClipitQuizResult::SUBTYPE;
-    $replace["clipit_rating"] = ClipitRating::SUBTYPE;
-    $replace["clipit_site"] = ClipitSite::SUBTYPE;
-    $replace["clipit_sta"] = ClipitSTA::SUBTYPE;
-    $replace["clipit_storyboard"] = ClipitStoryboard::SUBTYPE;
-    $replace["clipit_tag"] = ClipitTag::SUBTYPE;
-    $replace["clipit_tag_rating"] = ClipitTagRating::SUBTYPE;
-    $replace["clipot_tagrating"] = ClipitTagRating::SUBTYPE;
-    $replace["clipit_task"] = ClipitTask::SUBTYPE;
-    $replace["clipit_tricky_topic"] = ClipitTrickyTopic::SUBTYPE;
-    $replace["clipit_trickytopic"] = ClipitTrickyTopic::SUBTYPE;
-    $replace["clipit_video"] = ClipitVideo::SUBTYPE;
-    foreach($replace as $old => $new){
-        $query = "update {$CONFIG->dbprefix}entity_subtypes set subtype = \"{$new}\" where subtype = \"{$old}\";";
-        update_data($query);
+function load_performance_palette(){
+    if(get_config("performance_palette") == true){
+        return;
+    } else{
+        set_config("performance_palette", true);
+    }
+    $json_object = json_decode(file_get_contents(elgg_get_plugins_path() . "z02_clipit_api/performance_palette.json"), true);
+    if(!is_array($json_object) || key($json_object)!= "performance_palette"){
+        return false;
+    }
+    ClipitPerformanceItem::delete_all();
+    $performance_items = array();
+    foreach($json_object["performance_palette"] as $category_array){
+        foreach($category_array as $key => $val){
+            switch ($key){
+                case "category":
+                    $category = $val;
+                    break;
+                case "category_description":
+                    $category_description = $val;
+                    break;
+                case "items":
+                    foreach($val as $item) {
+                        $prop_value_array = array();
+                        $prop_value_array["category"] = $category;
+                        $prop_value_array["category_description"] = $category_description;
+                        foreach ($item as $key_2 => $val_2) {
+                            $prop_value_array[$key_2] = $val_2;
+                        }
+                        $performance_items[] = ClipitPerformanceItem::create($prop_value_array);
+                    }
+                    break;
+            }
+        }
     }
 }
 
