140201c57d134924b49754d1d1c1caaafbb25d1e
Pablo Llinás Arnaiz <pebs74@gmail.com>
2014-07-22 18:17:27 +0200

    new: class description comments

--- mod/z02_clipit_api/classes/ClipitTagRating.php
+++ mod/z02_clipit_api/classes/ClipitTagRating.php
@@ -13,7 +13,8 @@
  */
 
 /**
- * <Class Description>
+ * A rating of whether a Tag has correctly been covered in a Resource. It is contained inside of a ClipitRating instance
+ * which points to a specific Resource.
  */
 class ClipitTagRating extends UBItem {
     /**
