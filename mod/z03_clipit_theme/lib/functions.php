2dab68cf954890a6e0695fa3caad9bb24383b34f
magutierrez <magutierrezmoreno@gmail.com>
2014-09-01 17:22:18 +0200

    01/09/2014 17:22

--- mod/z03_clipit_theme/lib/functions.php
+++ mod/z03_clipit_theme/lib/functions.php
@@ -71,6 +71,28 @@ function get_friendly_time($time){
     }
 }
 
+/**
+ * Convert Hex Color to RGB
+ *
+ * @param $hex
+ * @return array
+ */
+function hex2rgb($hex) {
+    $hex = str_replace("#", "", $hex);
+
+    if(strlen($hex) == 3) {
+        $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+        $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+        $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+    } else {
+        $r = hexdec(substr($hex,0,2));
+        $g = hexdec(substr($hex,2,2));
+        $b = hexdec(substr($hex,4,2));
+    }
+    $rgb = array($r, $g, $b);
+    //return implode(",", $rgb); // returns the rgb values separated by commas
+    return $rgb; // returns an array with the rgb values
+}
 function clipit_events_feed($entity){
     $vars['author'] = array_pop(ClipitUser::get_by_id(array($entity->performed_by_guid)));
     $vars['time'] = $entity->time_created;
