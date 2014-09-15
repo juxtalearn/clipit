5286b987285d3b322e428c8be4a8f3c80cef087e
magutierrez <magutierrezmoreno@gmail.com>
2014-09-15 14:28:07 +0200

    15/09/2014 14:28

--- mod/z04_clipit_activity/lib/functions.php
+++ mod/z04_clipit_activity/lib/functions.php
@@ -361,7 +361,7 @@ function get_task_status(ClipitTask $task, $group_id = 0, $user_id = null){
 function get_group_progress($group_id){
     $activity_id = ClipitGroup::get_activity($group_id);
     $activity = array_pop(ClipitActivity::get_by_id(array($activity_id)));
-    $individual_tasks = array('video_feedback', 'storyboard_feedback', 'quiz_take');
+    $individual_tasks = array(ClipitTask::TYPE_VIDEO_FEEDBACK, ClipitTask::TYPE_STORYBOARD_FEEDBACK, ClipitTask::TYPE_QUIZ_TAKE);
     $completed = array();
     $total = 0;
     foreach($activity->task_array as $task_id){
