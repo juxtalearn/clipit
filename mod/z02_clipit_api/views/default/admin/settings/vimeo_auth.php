999331fe0f322e23358579d3fb06410bcb66c296
Pablo Llinás <pebs74@gmail.com>
2014-10-21 17:48:13 +0200

    added vimeo php api

---
+++ mod/z02_clipit_api/views/default/admin/settings/vimeo_auth.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+set_include_path(
+    get_include_path() . PATH_SEPARATOR . elgg_get_plugins_path() . "z02_clipit_api/libraries/vimeo_api/"
+);
+require_once 'vimeo.php';
+
+$app_id = get_config("vimeo_app_id");
+$app_secret = get_config("vimeo_app_secret");
+$access_token = get_config("vimeo_access_token");
+
+//  Create a handle for the Vimeo API, with the access token.
+$vimeo = new Vimeo($app_id, $app_secret, $access_token);
+
+var_dump($vimeo->upload("/var/www/html/dev_data/2014/10/02/498/1527056745"));
