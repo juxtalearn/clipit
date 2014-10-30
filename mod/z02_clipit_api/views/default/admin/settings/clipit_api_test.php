532cce7a4d02c189c04546a09c884ee83a4f4dae
pebs74 <pebs74@gmail.com>
2014-10-30 12:59:19 +0100

    testing php git pull

--- mod/z02_clipit_api/views/default/admin/settings/clipit_api_test.php
+++ mod/z02_clipit_api/views/default/admin/settings/clipit_api_test.php
@@ -15,8 +15,8 @@ switch($action) {
     case "git_pull":
         $clipit_path = elgg_get_root_path();
         chdir($clipit_path);
-        echo exec("git stash save \"auto stash\"");
-        echo exec("git pull --recurse-submodules");
+        echo exec("git stash save \"auto stash\"")."\n";
+        echo exec("git pull --recurse-submodules")."\n";
         echo "<h3 align = center> Succesfully pulled all the files.</h3>";
         break;
     case "export_users":
