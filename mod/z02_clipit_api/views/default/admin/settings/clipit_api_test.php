af67ea3861bddd4f4a7a78df7bdbe77ddadbbccc
pebs74 <pebs74@gmail.com>
2014-10-30 12:41:18 +0100

    testing php git pull

--- mod/z02_clipit_api/views/default/admin/settings/clipit_api_test.php
+++ mod/z02_clipit_api/views/default/admin/settings/clipit_api_test.php
@@ -13,12 +13,11 @@ if(isset($_GET["action"])) {
 }
 switch($action) {
     case "git_pull":
-        $path = "/var/www/repos/$_POST[project]"; 
-        $a='';
-        chdir($path);
-        exec("git add .");  
-        exec("git commit -m'message'");
-        echo "<h3 align = center> Succesfully commited all the files.</h3>";
+        $clipit_path = elgg_get_root_path();
+        chdir($clipit_path);
+        echo exec("git stash -m\"auto stash\"");
+        echo exec("git pull --recurse-submodules");
+        echo "<h3 align = center> Succesfully pulled all the files.</h3>";
         break;
     case "export_users":
         print_r(ClipitUser::export_data());
@@ -26,6 +25,5 @@ switch($action) {
     case "export_activities":
         print_r(ClipitActivity::export_data());
         break;
-
 }
 
