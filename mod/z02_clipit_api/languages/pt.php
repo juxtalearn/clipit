aafd4a4b8632312474e6f4622611e91ccb453b8f
pebs74 <pebs74@gmail.com>
2014-11-04 15:57:12 +0100

    added translations

--- mod/z02_clipit_api/languages/pt.php
+++ mod/z02_clipit_api/languages/pt.php
@@ -5,6 +5,7 @@
  */
 $portuguese = array(
     "admin:settings:youtube_auth" => "Configuracao do YouTube",
+    "admin:settings:vimeo_auth" => "Configuracao do Vimeo",
     "admin:settings:update_clipit" => "Update ClipIt",
     "student_option" => "Fazer deste utilizador um estudante?",
     "teacher_option" => "Fazer deste utilizador um professor?",
