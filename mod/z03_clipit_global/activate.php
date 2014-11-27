6a9e0d696811e2b1ed66af722a41bebd5bbf2f2e
magutierrez <magutierrezmoreno@gmail.com>
2014-11-27 15:57:29 +0100

    27/11/2014 15:51

---
+++ mod/z03_clipit_global/activate.php
@@ -0,0 +1,12 @@
+<?php
+ /**
+ * ClipIt - JuxtaLearn Web Space
+ * PHP version:     >= 5.2
+ * Creation date:   27/11/2014
+ * Last update:     27/11/2014
+ * @author          Miguel Ángel Gutiérrez <magutierrezmoreno@gmail.com>, URJC JuxtaLearn Project
+ * @version         $Version$
+ * @link            http://www.juxtalearn.eu
+ * @license         GNU Affero General Public License v3
+ * @package         ClipIt
+ */ 
