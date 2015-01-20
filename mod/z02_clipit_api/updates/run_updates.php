b2c3bf24e048cb2b0333c031187dfcd0dd7c1f2a
pebs74 <pebs74@gmail.com>
2015-01-20 18:07:09 +0100

    added patching mechanism to clipit update

--- mod/z02_clipit_api/updates/run_updates.php
+++ mod/z02_clipit_api/updates/run_updates.php
@@ -2,6 +2,8 @@
 $VERSION = "2.2.1";
 $old_version = get_config("clipit_version");
 
+print_r("<p>Current version: $old_version<br>New version: $VERSION</p>");
+
 if($VERSION === $old_version) return;
 
 // list of update files for each version
@@ -10,18 +12,24 @@ $update_files = array(
     "2.2.1" => "update_2.2.1.php",
 );
 
-// advance until old version
-while(key($update_files) != $old_version) next($update_files);
-// skip old version update file
-next($update_files);
+if(!empty($old_version)) {
+    // advance until old version
+    while (key($update_files) != $old_version) {
+        next($update_files);
+    }
+    // skip old version update file
+    next($update_files);
+}
 // apply all updates from there onwards
 while(key($update_files) != null){
     $value = current($update_files);
     if(!empty($value)){
         include_once((string)$value);
+        print_r("<p>Applied patch: $value</p>");
     }
     next($update_files);
 };
-
 set_config("clipit_version", $VERSION);
+print_r("<p>Updated to version: $VERSION</p>");
+
 
