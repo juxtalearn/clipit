cc6f374ee5358c4dca45f37d63f6483f77dfb2e7
pebs74 <pebs74@gmail.com>
2015-01-20 17:45:49 +0100

    added patching mechanism to clipit update

--- mod/z02_clipit_api/views/default/admin/settings/update_clipit.php
+++ mod/z02_clipit_api/views/default/admin/settings/update_clipit.php
@@ -1,4 +1,5 @@
 <?php
+// Pull latest version from GitHub
 chdir(elgg_get_root_path());
 echo "<h3>Performing local git stash... ";
 echo exec("git stash save \"auto stash\"");
@@ -7,3 +8,6 @@ echo exec("git pull");
 echo "<h3>Performing submodule update... ";
 echo exec("git submodule init");
 echo exec("git submodule update");
+
+// Run updates
+include_once(elgg_get_plugins_path()."z02_clipit_api/updates/run_updates.php");
