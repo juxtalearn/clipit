ace32866ef88bc35415835c6234be6a2330aa0b4
pebs74 <KrisYPebs2014>
2015-03-05 17:13:20 +0100

    reactivate all plugins on update 2.3.6

--- mod/z02_clipit_api/updates/update_2.3.6.php
+++ mod/z02_clipit_api/updates/update_2.3.6.php
@@ -7,3 +7,8 @@
  */
 // disable new student registration by default
 set_config('allow_registration', 0);
+// z99_clipit_demo changed to z20_clipit_demo, so reactivate all plugins
+$plugins = elgg_get_plugins('inactive');
+foreach ($plugins as $plugin){
+    $plugin->activate();
+}
