1c7611bcdaa037ea9957619f76934d66ee5e5bd1
pebs74 <KrisYPebs2014>
2015-03-05 11:34:14 +0100

    update_clipit: added confirmation message improved translations

--- mod/z02_clipit_api/views/default/admin/settings/update_clipit.php
+++ mod/z02_clipit_api/views/default/admin/settings/update_clipit.php
@@ -1,17 +1,22 @@
 <?php
-// Pull latest version from GitHub
-chdir(elgg_get_root_path());
-echo "<h2>Updating ClipIt to latest release";
-echo "<h3>Performing local git stash... ";
-echo exec("git stash save \"auto stash\"");
-echo "<h3>Fetching latest tag info... ";
-echo exec("git fetch --tags");
-echo "<h3>Checking-out latest tag";
-echo exec("git checkout `git tag | tail -1`");
-echo "<h3>Performing submodule update... ";
-echo exec("git submodule init");
-echo exec("git submodule update");
-echo "<h3>Update complete to latest release";
-
-// Run updates
-include_once(elgg_get_plugins_path()."z02_clipit_api/updates/run_updates.php");
+session_start();
+if($_POST["update_clipit"] == "YES") {
+    // Pull latest version from GitHub
+    chdir(elgg_get_root_path());
+    echo "<h2>Updating ClipIt to latest release";
+    echo "<h3>Performing local git stash... ";
+    echo exec("git stash save \"auto stash\"");
+    echo "<h3>Fetching latest tag info... ";
+    echo exec("git fetch --tags");
+    echo "<h3>Checking-out latest tag";
+    echo exec("git checkout `git tag | tail -1`");
+    echo "<h3>Performing submodule update... ";
+    echo exec("git submodule init");
+    echo exec("git submodule update");
+    echo "<h3>Update complete to latest release";
+    // Run updates
+    include_once(elgg_get_plugins_path() . "z02_clipit_api/updates/run_updates.php");
+}else{
+    echo "<h2>The update procedure will checkout the latest ClipIt version and apply all necessary updates.</h2>";
+    echo "<br/><form action='update_clipit' method='post'><h3>Are you sure you want to proceed? <input name='update_clipit' value='YES' type='submit'></h3></form>";
+}
