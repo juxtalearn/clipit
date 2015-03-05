760e859ed2ffe8fb7675d3076bce7a583cf05e5e
magutierrez <magutierrezmoreno@gmail.com>
2015-03-05 11:47:14 +0100

    login admin page created

--- mod/z99_clipit_demo/views/default/forms/demo/login.php
+++ mod/z99_clipit_demo/views/default/forms/demo/login.php
@@ -54,39 +54,4 @@ $lang = $CONFIG->language;
         </label>
     </div>
 <?php endif;?>
-<div style="display: none;">
-    <div>
-        <label><?php echo elgg_echo('loginusername'); ?></label>
-        <?php echo elgg_view('input/text', array(
-            'name' => 'username',
-            'class' => 'form-control input-lg',
-        ));
-        ?>
-    </div>
-    <div>
-        <label><?php echo elgg_echo('password'); ?></label>
-        <?php echo elgg_view('input/password',
-            array(
-                'name' => 'password',
-                'class' => 'form-control input-lg'
-            ));
-        ?>
-    </div>
-    <div>
-        <label style="margin: 10px 0px 0px">
-            <input type="checkbox" name="persistent" value="true" />
-            <?php echo elgg_echo('user:persistent'); ?>
-        </label>
-    </div>
-    <?php echo elgg_view('login/extend', $vars); ?>
-    <div class="elgg-foot">
-        <?php echo elgg_view('input/submit', array('value' => elgg_echo('login'), 'class'=>'btn btn-primary btn-lg')); ?>
 
-        <?php
-        if (isset($vars['returntoreferer'])) {
-            echo elgg_view('input/hidden', array('name' => 'returntoreferer', 'value' => 'true'));
-        }
-        ?>
-
-    </div>
-</div>
