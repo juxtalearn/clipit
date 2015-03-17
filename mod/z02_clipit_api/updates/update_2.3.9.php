bfc3daa5a5145af3e6f32eeba33256223e4f9980
pebs74 <KrisYPebs2014>
2015-03-17 17:44:55 +0100

    - Optimized updating rating average for Videos and Storyboards - Force initial update of average ratings

---
+++ mod/z02_clipit_api/updates/update_2.3.9.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pebs74
+ * Date: 20/01/2015
+ * Time: 16:55
+ */
+// initial update of average ratings
+$video_array = ClipitVideo::get_all();
+foreach($video_array as $video){
+    ClipitVideo::update_average_ratings($video->id);
+}
+$storyboard_array = ClipitStoryboard::get_all();
+foreach($storyboard_array as $storyboard){
+    ClipitStoryboard::update_average_ratings($storyboard->id);
+}
