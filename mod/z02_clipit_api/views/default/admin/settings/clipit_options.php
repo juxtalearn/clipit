1b83b73a67e238b65ea7a7ab7309bf9937056419
Pablo Llinás <pebs74@gmail.com>
2015-04-09 18:08:47 +0200

    new ClipIt Options page in Admin Settings ClipitSite: site type constants added ClipitTask: added performance_item_array for fixed items by teacher option

--- mod/z02_clipit_api/views/default/admin/settings/clipit_options.php
+++ mod/z02_clipit_api/views/default/admin/settings/clipit_options.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 
-elgg_echo("<h2>admin:settings:clipit_settings</h2>");
-
 // Save posted options
 if(isset($_POST["submit_options"])) {
     set_config("allow_registration", (bool)$_POST["allow_registration"]);
@@ -15,6 +13,7 @@ if(isset($_POST["submit_options"])) {
     set_config("performance_palette", (bool)$_POST["performance_palette"]);
     set_config("example_types", (bool)$_POST["example_types"]);
     set_config("fixed_performance_rating", (bool)$_POST["fixed_performance_rating"]);
+    system_message("The options have been saved correctly");
 }
 
 // Fetch current options
@@ -32,13 +31,15 @@ $fixed_performance_rating = (bool)get_config("fixed_performance_rating");
 // Main options form
 echo "<form action='clipit_options' method='post'>";
 
-echo "<h3>General Options</h3>";
+echo "<h3>General Options</h3><br>";
 
-echo "<p><h4>Site time zone (see <a href='http://www.php.net/manual/en/timezones.php' target='_blank'>PHP Time Zones</a>):</h4>";
+echo "<div style='margin-left:20px;text-indent:-10px'>";
+
+echo "<p><strong>Site time zone (see <a href='http://www.php.net/manual/en/timezones.php' target='_blank'>PHP Time Zones</a>)</strong><br>";
 echo "<input name='timezone' value=$timezone type='text' size='60'>";
 echo "</p>";
 
-echo "<p><h4>Site type:</h4>";
+echo "<p><strong>Site type</strong><br>";
 switch($clipit_site_type){
     case ClipitSite::TYPE_SITE:
         echo "<input name='clipit_site_type' value=".ClipitSite::TYPE_SITE." type='radio' checked>".ClipitSite::TYPE_SITE."<br>";
@@ -58,32 +59,40 @@ switch($clipit_site_type){
 }
 echo "</p>";
 
-echo "<p><h4>Global site setup:</h4>";
-echo "<input name='clipit_global_url' value=$clipit_global_url type='text' size='60'><br>";
-echo "<input name='clipit_global_login' value=$clipit_global_login type='text' size='60'><br>";
-echo "<input name='clipit_global_password' value=$clipit_global_password type='text' size='60'>";
+echo "<p><strong>Global site setup</strong><br>";
+echo "URL:<br><input name='clipit_global_url' value=$clipit_global_url type='text' size='60'><br>";
+echo "Login:<br><input name='clipit_global_login' value=$clipit_global_login type='text' size='60'><br>";
+echo "Password:<br><input name='clipit_global_password' value=$clipit_global_password type='text' size='60'>";
 echo "</p>";
 
-echo "<p><h4>Republish this site to global site?</h4>";
+echo "<p><strong>Republish this site to global site?</strong><br>";
 echo "<input name='clipit_global_published' value='0' type='radio'> yes<br>";
 echo "<input name='clipit_global_published' value='1' type='radio' checked> no";
 echo "</p>";
 
-echo "<h3>Authoring Tool Options</h3>";
+echo "</div>";
 
-echo "<p><h4>Reload performance palette?</h4>";
+echo "<h3>Authoring Tool Options</h3><br>";
+
+echo "<div style='margin-left:20px;text-indent:-10px'>";
+
+echo "<p><strong>Reload performance palette?</strong><br>";
 echo "<input name='performance_palette' value='0' type='radio'> yes<br>";
 echo "<input name='performance_palette' value='1' type='radio' checked> no";
 echo "</p>";
 
-echo "<p><h4>Reload problem example palette?</h4>";
+echo "<p><strong>Reload problem example palette?</strong><br>";
 echo "<input name='example_types' value='0' type='radio'> yes<br>";
 echo "<input name='example_types' value='1' type='radio' checked> no";
 echo "</p>";
 
-echo "<h3>Activity Options</h3>";
+echo "</div>";
 
-echo "<p><h4>Allow students to self-register?</h4>";
+echo "<h3>Activity Options</h3><br>";
+
+echo "<div style='margin-left:20px;text-indent:-10px'>";
+
+echo "<p><strong>Allow students to create an account in ClipIt?</strong><br>";
 if($allow_registration){
     echo "<input name='allow_registration' value='1' type='radio' checked> yes<br>";
     echo "<input name='allow_registration' value='0' type='radio'> no";
@@ -93,7 +102,7 @@ if($allow_registration){
 }
 echo "</p>";
 
-echo "<p><h4>Teachers select performance items for feedback tasks?</h4>";
+echo "<p><strong>Teachers select performance items for feedback tasks?</strong><br>";
 if($fixed_performance_rating) {
     echo "<input name='fixed_performance_rating' value='1' type='radio' checked> yes<br>";
     echo "<input name='fixed_performance_rating' value='0' type='radio'> no";
@@ -102,6 +111,8 @@ if($fixed_performance_rating) {
     echo "<input name='fixed_performance_rating' value='0' type='radio' checked> no";
 }
 echo "</p>";
+
+echo "</div>";
 
 echo "<input name='submit_options' type='submit' value='Save'>";
 
