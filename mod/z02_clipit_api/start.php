09ed4f1a53cef925602781919c5a1e45b460400b
Pablo Llinás <pebs74@gmail.com>
2015-05-08 19:23:03 +0200

    create new plugin hook for permissions_check so that users from a same group can edit all material in the group repository

--- mod/z02_clipit_api/start.php
+++ mod/z02_clipit_api/start.php
@@ -38,4 +38,33 @@ function clipit_api_init() {
         set_config("clipit_global_published", true);
         ClipitSite::publish_to_global();
     }
+    // Add aditional plugin hook handler for permissions_check
+    elgg_register_plugin_hook_handler('permissions_check', 'all', 'clipit_override_permissions',600);
+}
+
+function clipit_override_permissions($hook, $type, $value, $params){
+    // check if user is inside the same ClipitGroup where the entity is contained
+    $user = elgg_extract('user', $params);
+    $entity = elgg_extract('entity', $params);
+    $entity_subtype = get_subtype_from_id($entity->subtype);
+    switch ($entity_subtype){
+        case ClipitVideo::SUBTYPE:
+            $entity_group = ClipitVideo::get_group($entity->guid);
+            break;
+        case ClipitStoryboard::SUBTYPE:
+            $entity_group = ClipitStoryboard::get_group($entity->guid);
+            break;
+        case ClipitFile::SUBTYPE:
+            $entity_group = ClipitFile::get_group($entity->guid);
+            break;
+        default:
+            $entity_group = null;
+    }
+    if(!empty($entity_group)){
+        $user_groups = ClipitUser::get_groups($user->guid);
+        if(in_array($entity_group, $user_groups)){
+            return true;
+        }
+    }
+    return null;
 }
